f43b3638bd0837f2ebc144d19ada2cd5c420811f
ankit417 <acharya.ankit417@gmail.com>
2023-03-20 22:11:15 +0000

    bst

---
+++ bst-dev/bst.js
@@ -0,0 +1,63 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    var newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(root, newNode) {
+    if (newNode.value < root.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
+
+  search(root, value) {
+    if (!root) {
+      return false;
+    } else {
+      if (root.value === value) {
+        return true;
+      } else if (value < root.value) {
+        return this.search(root.left, value);
+      } else {
+        return this.search(root.right, value);
+      }
+    }
+  }
+}
+
+const bst = new BST();
+
+bst.insert(10);
+bst.insert(11);
+bst.insert(9);
+bst.insert(8);
+
+console.log("BST", bst);
+console.log("bst search", bst.search(bst.root, 91));
