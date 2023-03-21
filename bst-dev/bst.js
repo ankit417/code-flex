54cbf0186c7dbaa9f742a1ca37ab9a8b529f3bcf
ankit417 <acharya.ankit417@gmail.com>
2023-03-21 20:03:35 +0000

    preorder traversal

--- bst-dev/bst.js
+++ bst-dev/bst.js
@@ -50,6 +50,14 @@ class BST {
       }
     }
   }
+
+  preOrder(root) {
+    if (root) {
+      console.log(root.value);
+      this.preOrder(root.left);
+      this.preOrder(root.right);
+    }
+  }
 }
 
 const bst = new BST();
